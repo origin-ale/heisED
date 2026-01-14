cb78e6fc4e780da0af3c26279c420fb5b96aa63a
Alessandro Cerati <alessandro.cerati@studio.unibo.it>
2026-01-14 16:12:30 +0100

    Iterate through multiple lengths, save results to file

---
+++ file_output.py
@@ -0,0 +1,23 @@
+def grounds_to_file(lengths, energies, en_lens, durations, J):
+  """
+  Save three lists as columns in a text file.
+  
+  Parameters
+  ---
+    lengths: list
+      The lengths of the Heisenberg models
+    energies: list
+      The GS energies of the Heisenberg models
+    en_lens: list
+      The energy/lengths of the Heisenberg models
+    durations: list
+      The durations in seconds of each diagonalization
+    J: float
+      The magnetic field. Used as filename and saved in header
+  """
+  filename = f"J{J:.5f}.txt"
+  
+  with open(filename, 'w') as f:
+    f.write(f"Heisenberg model lengths, energies, E/L and time to diagonalize (s) in J={J}\n")
+    for item1, item2, item3, item4 in zip(lengths, energies, en_lens, durations):
+      f.write(f"{item1}\t{item2:.5f}\t{item3:.5f}\t{item4:.5f}\n")
--- main.py
+++ main.py
@@ -3,19 +3,46 @@ from scipy.linalg import eigh_tridiagonal
 
 from build_hamiltonian import build_hamiltonian
 from lanczos import spinH_lanczos
+from file_output import grounds_to_file
 import mytiming as mt
 
-L = 12
 J = 1.
+L = 1
 
-H = build_hamiltonian(L, J)
+iter_duration = 0. # How long the last iteration lasted
+max_duration = 300 # Maximum time for an iteration before program is set to stop, in seconds
 
-tridiag = spinH_lanczos(H)
+lengths = []
+energies = []
+en_lens = []
+durations = []
 
-gs_energy = eigh_tridiagonal(tridiag.diagonal(), 
-                             tridiag.diagonal(offset=1), 
-                             eigvals_only=True, 
-                             select='i', 
-                             select_range=[0,0])
+print(16*'=' + f" Running with J = {J} until iterations take {max_duration} s " + 16*'=')
+while(iter_duration < max_duration):
+  iter_start = mt.perf_counter()
 
-print(f'{L} spins with PBCs, J={J}:\n\tE0 {gs_energy[0]:.7f}\n\tE0/L {gs_energy[0]/L:.7f}')
+  print(16*'=' + f" L = {L} " + 16*'=')
+  H = build_hamiltonian(L, J)
+
+  tridiag = spinH_lanczos(H)
+
+  gs_energy = eigh_tridiagonal(tridiag.diagonal(), 
+                              tridiag.diagonal(offset=1), 
+                              eigvals_only=True, 
+                              select='i', 
+                              select_range=[0,0])
+  gs = gs_energy[0]
+  print(f'{L} spins with PBCs, J={J}:\n\tE0 {gs:.7f}\n\tE0/L {gs/L:.7f}')
+
+  iter_end = mt.perf_counter()
+  iter_duration = iter_end - iter_start
+
+  lengths.append(L)
+  energies.append(gs)
+  en_lens.append(gs/L)
+  durations.append(iter_duration)
+
+  L += 1
+
+print(16*'=' + f" Maximum duration {max_duration} s passed at L = {L-1} " + 16*'=')
+grounds_to_file(lengths, energies, en_lens, durations, J)
